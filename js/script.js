a60e9b2d485f075322d284e769c7ccc559acd382
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 19:54:36 +0530

    Update script.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 function runCode(){
-    const codeHtml = document.getElementById('for-html');
-    const codeCss = document.getElementById('for-css');
-    const codeJS = document.getElementById('for-js');
+    const codeHtml = document.getElementById('for-html').value;
+    const codeCss = document.getElementById('for-css').value;
+    const codeJS = document.getElementById('for-js').value;
     const codeResult = document.getElementById('result-code');
+
+    codeResult.contentDocument.body.innerHTML = codeHtml+"<style>"+codeCss+"</style>";
+    codeResult.contentWindow.eval(codeJS);
     
 }
