fc3d4fc2000ba6328fde72b1e0c05b18bfd8f147
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-21 19:51:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function runCode(){
+    const codeHtml = document.getElementById('for-html');
+    const codeCss = document.getElementById('for-css');
+    const codeJS = document.getElementById('for-js');
+    const codeResult = document.getElementById('result-code');
+    
+}
